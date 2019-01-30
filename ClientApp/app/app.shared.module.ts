b3a5dcfb7a61757f4939cd4ef3630c64bea96c39
DiegoMullerP <diegomp86@gmail.com>
2019-01-30 13:29:22 -0200

    first

--- ClientApp/app/app.shared.module.ts
+++ ClientApp/app/app.shared.module.ts
@@ -8,13 +8,14 @@ import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
+import { TruckComponent } from './components/truck/truck.component';
+import { TruckService } from './components/truck/truck.service';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
+        TruckComponent,
         FetchDataComponent,
         HomeComponent
     ],
@@ -25,7 +26,7 @@ import { CounterComponent } from './components/counter/counter.component';
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
+            { path: 'truck', component: TruckComponent },
             { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
